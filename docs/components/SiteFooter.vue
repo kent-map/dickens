d9006475caa9abb15be10be64d68401f36753233
Ron Snyder <Ronald.Snyder@ithaka.org>
2020-05-06 16:19:42 -0400

    Restructuring

---
+++ docs/components/SiteFooter.vue
@@ -0,0 +1,29 @@
+<template>
+    <v-row>
+      <v-col cols="12" nogutter>
+        <span style="padding-bottom: 8px; font-weight: bold; display: block">A collaboration between:</span>
+        <img src="https://kent-map.github.io/dickens/images/CCCU-logo-2colour.svg" height="30px"> &nbsp;&nbsp;&nbsp;&nbsp;
+        <img src="https://kent-map.github.io/dickens/images/labs.jpg" height="30px">
+      </v-col>
+    </v-row>
+</template>
+
+<script>
+  module.exports = {  
+    data: () => ({})
+  }
+</script>
+
+<style>
+  
+  [v-cloak] { display: none; }
+  
+  .site-footer, .row {
+    padding: 0;
+    margin: 0;
+  }
+  .col {
+    padding: 0 16px;
+  }
+
+</style>
